405a963ff494e5379581001e4f62b4b1cd81f9ef
Max Hao <Max.Hao@D2L.com>
2020-10-21 11:12:55 -0400

    Test: Remove userProgressOutcomeHref from _resetEvidence

--- components/consistent-evaluation-page.js
+++ components/consistent-evaluation-page.js
@@ -325,7 +325,7 @@ export default class ConsistentEvaluationPage extends LocalizeMixin(LitElement) 
 		this.submissionInfo = undefined;
 		this._fileEvidenceUrl = undefined;
 		this._textEvidence = undefined;
-		this.userProgressOutcomeHref = undefined;
+		//this.userProgressOutcomeHref = undefined;
 	}
 
 	_hideScrollbars() {
