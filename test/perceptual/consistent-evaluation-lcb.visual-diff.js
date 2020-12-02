2de12e2c97cdd2c9f4f832d1960c24727db0f587
Jamie Tran <Jamie.Tran@D2L.com>
2020-12-02 14:45:02 -0500

    :white_check_mark: Add skeleton test and fix file-context-bar issues

--- test/perceptual/consistent-evaluation-lcb.visual-diff.js
+++ test/perceptual/consistent-evaluation-lcb.visual-diff.js
@@ -8,9 +8,12 @@ describe('d2l-consistent-evaluation', () => {
 	let browser, page;
 
 	before(async() => {
-		browser = await puppeteer.launch();
+		browser = await puppeteer.launch({
+			headless: true,
+			args: ['--no-sandbox', '--disable-setuid-sandbox', '--lang=en-GB']
+		});
 		page = await browser.newPage();
-		await page.setViewport({width: 900, height: 900, deviceScaleFactor: 2});
+		await page.setViewport({width: 1000, height: 2000, deviceScaleFactor: 2});
 		await page.goto(`${visualDiff.getBaseUrl()}/test/perceptual/consistent-evaluation-lcb.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
 		await visualDiff.disableAnimations(page);
@@ -18,12 +21,17 @@ describe('d2l-consistent-evaluation', () => {
 
 	after(async() => await browser.close());
 
+	it('renders learner context bar with skeleton', async function() {
+		const rect = await visualDiff.getRect(page, '#skeleton');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
 	it('renders learner context bar', async function() {
 		const rect = await visualDiff.getRect(page, '#default');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
-	it('renders exempt learner context bar', async function() {
+	it('renders learner context bar with exempt', async function() {
 		const rect = await visualDiff.getRect(page, '#is-exempt');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
