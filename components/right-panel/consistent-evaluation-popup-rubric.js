4c6c95507d758cacc5044632fecf3fc2b193bf35
Jamie Tran <Jamie.Tran@D2L.com>
2020-12-14 13:24:45 -0500

    :construction: WIP for popup-rubric

---
+++ components/right-panel/consistent-evaluation-popup-rubric.js
@@ -0,0 +1,23 @@
+
+import { css, html, LitElement } from 'lit-element';
+
+export class ConsistentEvaluationPopupRubric extends LitElement {
+
+	static get properties() {
+		return {
+		};
+	}
+
+	static get styles() {
+		return css`
+		`;
+	}
+
+	render() {
+		//rubrics will appear here
+		return html`
+		`;
+	}
+}
+
+customElements.define('d2l-consistent-evaluation-popup-rubric', ConsistentEvaluationPopupRubric);
