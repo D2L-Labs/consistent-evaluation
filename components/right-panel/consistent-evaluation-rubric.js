9615bec0519a86d9dea23056456be09d0507b2e3
Jamie Murray <Jamie.Murray@D2L.com>
2020-12-17 13:53:57 -0500

    - Setting no scoring rubric to the default value of -1

--- components/right-panel/consistent-evaluation-rubric.js
+++ components/right-panel/consistent-evaluation-rubric.js
@@ -131,7 +131,7 @@ class ConsistentEvaluationRubric extends LocalizeConsistentEvaluation(LitElement
 		return html`
 			<h2 class="d2l-label-text">${this.localize('gradingRubric')}</h2>
 			<select class="d2l-input-select d2l-truncate d2l-consistent-evaluation-active-scoring-rubric" aria-label=${this.localize('activeGradingRubric')} @change=${this._onActiveScoringRubricChange}>
-				<option label=${this.localize('noActiveGradingRubric')} ?selected=${!this.activeScoringRubric} value=null></option>
+				<option label=${this.localize('noActiveGradingRubric')} ?selected=${!this.activeScoringRubric} value="-1"></option>
 				${scoringRubrics.map(rubric => html`
 						<option value="${rubric.rubricId}" label=${rubric.rubricTitle} class="select-option"></option>
 				`)}
