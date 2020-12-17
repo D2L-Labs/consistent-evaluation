02389090baec74c6f492af9e162821cafdce99c5
Jamie Murray <Jamie.Murray@D2L.com>
2020-12-17 14:46:33 -0500

    - Whitespace changes

--- components/right-panel/consistent-evaluation-rubric.js
+++ components/right-panel/consistent-evaluation-rubric.js
@@ -137,11 +137,11 @@ class ConsistentEvaluationRubric extends LocalizeConsistentEvaluation(LitElement
 		}
 		return html`
 			<h2 class="d2l-label-text">${this.localize('gradingRubric')}</h2>
-        <select class="d2l-input-select d2l-consistent-evaluation-active-scoring-rubric" aria-label=${this.localize('activeGradingRubric')} @change=${this._onActiveScoringRubricChange}>
-          <option label=${this.localize('noActiveGradingRubric')} ?selected=${!this.activeScoringRubric} value='-1'></option>
-          ${scoringRubrics.map(rubric => html`
-            <option value="${rubric.rubricId}" label=${this._truncateRubricTitle(rubric.rubricTitle)} class="select-option"></option>
-				  `)}
+			<select class="d2l-input-select d2l-consistent-evaluation-active-scoring-rubric" aria-label=${this.localize('activeGradingRubric')} @change=${this._onActiveScoringRubricChange}>
+				<option label=${this.localize('noActiveGradingRubric')} ?selected=${!this.activeScoringRubric} value='-1'></option>
+				${scoringRubrics.map(rubric => html`
+					<option value="${rubric.rubricId}" label=${this._truncateRubricTitle(rubric.rubricTitle)} class="select-option"></option>
+				`)}
 			</select>
 		`;
 	}
