1a9f91230dd67eb43e6235556feb398345f51721
Jamie Tran <Jamie.Tran@D2L.com>
2020-12-23 11:56:22 -0500

    :white_check_mark: Add vdiff tests for rubric popout

---
+++ test/perceptual/consistent-evaluation-popup-rubric.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-consistent-evaluation-popup-rubric', () => {
+
+	const visualDiff = new VisualDiff('consistent-evaluation-popup-rubric', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch({
+			headless: true,
+			args: ['--no-sandbox', '--disable-setuid-sandbox', '--lang=en-GB']
+		});
+		page = await visualDiff.createPage(browser, { viewport: { width: 1000, height: 1000 } });
+		await page.goto(`${visualDiff.getBaseUrl()}/test/perceptual/consistent-evaluation-popup-rubric.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
+		await page.bringToFront();
+	});
+
+	after(async() => await browser.close());
+
+	it('renders rubric popout', async function() {
+		const rect = await visualDiff.getRect(page, '#default');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+	it('renders rubric popout', async function() {
+		const rect = await visualDiff.getRect(page, '#multiple-rubrics');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+});
