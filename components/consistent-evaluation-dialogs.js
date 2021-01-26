45fbd10588994bed5b2d637f1fc9d270dce22564
mbosiljevac <Megan.Bosiljeac@d2l.com>
2021-01-26 11:23:15 -0500

    US124085/mbosilj don't render multiple dialogs.

--- components/consistent-evaluation-dialogs.js
+++ components/consistent-evaluation-dialogs.js
@@ -4,6 +4,8 @@ import { ConsistentEvaluationHrefController } from './controllers/ConsistentEval
 import { LocalizeConsistentEvaluation } from '../lang/localize-consistent-evaluation.js';
 
 const DIALOG_ACTION_CONTINUE_GRADING = 'continue_grading';
+const PUBLISH_UNSCORED_DIALOG = 'publishUnscored';
+const UPDATE_UNSCORED_DIALOG = 'updateUnscored';
 
 export class ConsistentEvaluationDialogs extends LocalizeConsistentEvaluation(LitElement) {
 
@@ -22,13 +24,14 @@ export class ConsistentEvaluationDialogs extends LocalizeConsistentEvaluation(Li
 				type: Boolean
 			},
 			token: {
+				attribute: false,
 				type: Object
 			},
-			_publishUnscoredCriteriaDialogOpened: {
-				type: Boolean,
-				attribute: false
+			_dialogToOpen: {
+				attribute: false,
+				type: String
 			},
-			_updateUnscoredCriteriaDialogOpened: {
+			_unscoredCriteriaDialogOpened: {
 				type: Boolean,
 				attribute: false
 			}
@@ -45,40 +48,22 @@ export class ConsistentEvaluationDialogs extends LocalizeConsistentEvaluation(Li
 	updated(changedProperties) {
 		super.updated(changedProperties);
 		if (changedProperties.has('publishClicked') && this.publishClicked) {
-			this._checkAndPublishEvaluation();
+			this._dialogToOpen = PUBLISH_UNSCORED_DIALOG;
+			this._showUnscoredCriteriaDialog();
 		} else if (changedProperties.has('updateClicked') && this.updateClicked) {
-			this._checkAndUpdateEvaluation();
+			this._dialogToOpen = UPDATE_UNSCORED_DIALOG;
+			this._showUnscoredCriteriaDialog();
 		}
 	}
 
-	async _checkAndPublishEvaluation() {
-		window.dispatchEvent(new CustomEvent('d2l-flush', {
-			composed: true,
-			bubbles: true
-		}));
-
+	async _showUnscoredCriteriaDialog() {
 		const hasUnscoredCriteria = await this._checkUnscoredCriteria();
 		if (hasUnscoredCriteria) {
-			this._publishUnscoredCriteriaDialogOpened = true;
+			this._unscoredCriteriaDialogOpened = true;
 			return;
 		}
 
-		this._firePublishEvaluationEvent();
-	}
-
-	async _checkAndUpdateEvaluation() {
-		window.dispatchEvent(new CustomEvent('d2l-flush', {
-			composed: true,
-			bubbles: true
-		}));
-
-		const hasUnscoredCriteria = await this._checkUnscoredCriteria();
-		if (hasUnscoredCriteria) {
-			this._updateUnscoredCriteriaDialogOpened = true;
-			return;
-		}
-
-		this._fireUpdateEvaluationEvent();
+		this._updateOrPublishEvaluation();
 	}
 
 	async _checkUnscoredCriteria() {
@@ -88,21 +73,20 @@ export class ConsistentEvaluationDialogs extends LocalizeConsistentEvaluation(Li
 		return hasUnscoredCriteria;
 	}
 
-	async _onPublishUnscoredCriteriaDialogClosed(e) {
-		this._publishUnscoredCriteriaDialogOpened = false;
-		if (e.detail.action !== DIALOG_ACTION_CONTINUE_GRADING) {
-			this._firePublishEvaluationEvent();
-		} else {
+	_onUnscoredCriteriaDialogClosed(e) {
+		this._unscoredCriteriaDialogOpened = false;
+		if (e.detail.action === DIALOG_ACTION_CONTINUE_GRADING) {
 			this._fireDialogClosedEvent();
+		} else {
+			this._updateOrPublishEvaluation();
 		}
 	}
 
-	async _onUpdateUnscoredCriteriaDialogClosed(e) {
-		this._updateUnscoredCriteriaDialogOpened = false;
-		if (e.detail.action !== DIALOG_ACTION_CONTINUE_GRADING) {
+	_updateOrPublishEvaluation() {
+		if (this._dialogToOpen === PUBLISH_UNSCORED_DIALOG) {
+			this._firePublishEvaluationEvent();
+		} else if (this._dialogToOpen === UPDATE_UNSCORED_DIALOG) {
 			this._fireUpdateEvaluationEvent();
-		} else {
-			this._fireDialogClosedEvent();
 		}
 	}
 
@@ -121,32 +105,28 @@ export class ConsistentEvaluationDialogs extends LocalizeConsistentEvaluation(Li
 	}
 
 	async _fireDialogClosedEvent() {
-		this.dispatchEvent(new CustomEvent('d2l-unscored-criteria-dialog-closed', {
+		this.dispatchEvent(new CustomEvent('d2l-dialog-closed', {
 			composed: true,
 			bubbles: true
 		}));
 	}
 
-	render() {
-		return html`
-			<d2l-dialog-confirm
-				title-text=${this.localize('unscoredCriteriaTitle')}
-				text=${this.localize('unscoredCriteriaBody')}
-				?opened=${this._publishUnscoredCriteriaDialogOpened}
-				@d2l-dialog-close=${this._onPublishUnscoredCriteriaDialogClosed}>
-					<d2l-button slot="footer" primary data-dialog-action=${DIALOG_ACTION_CONTINUE_GRADING}>${this.localize('continueGrading')}</d2l-button>
-					<d2l-button slot="footer" data-dialog-action>${this.localize('publish')}</d2l-button>
-			</d2l-dialog-confirm>
-			<d2l-dialog-confirm
-				title-text=${this.localize('unscoredCriteriaTitle')}
-				text=${this.localize('unscoredCriteriaBody')}
-				?opened=${this._updateUnscoredCriteriaDialogOpened}
-				@d2l-dialog-close=${this._onUpdateUnscoredCriteriaDialogClosed}>
-					<d2l-button slot="footer" primary data-dialog-action=${DIALOG_ACTION_CONTINUE_GRADING}>${this.localize('continueGrading')}</d2l-button>
-					<d2l-button slot="footer" data-dialog-action>${this.localize('update')}</d2l-button>
-			</d2l-dialog-confirm>
-		`;
+	_getUnscoredCriteriaAction() {
+		if (this._dialogToOpen === UPDATE_UNSCORED_DIALOG) {
+			return this.localize('update');
+		}
+		return this.localize('publish');
 	}
 
+	render() {
+		return html`<d2l-dialog-confirm
+				title-text=${this.localize('unscoredCriteriaTitle')}
+				text=${this.localize('unscoredCriteriaBody')}
+				?opened=${this._unscoredCriteriaDialogOpened}
+				@d2l-dialog-close=${this._onUnscoredCriteriaDialogClosed}>
+					<d2l-button slot="footer" primary data-dialog-action=${DIALOG_ACTION_CONTINUE_GRADING}>${this.localize('continueGrading')}</d2l-button>
+					<d2l-button slot="footer" data-dialog-action>${this._getUnscoredCriteriaAction()}</d2l-button>
+			</d2l-dialog-confirm>`;
+	}
 }
 customElements.define('d2l-consistent-evaluation-dialogs', ConsistentEvaluationDialogs);
